30a64baa4958653bdde45b4d34b21d14098969bc
nicolesaad5 <nano.saad5@gmail.com>
2025-11-13 20:12:39 +0200

    2.2 a VIEW

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -43,3 +43,12 @@ CREATE TABLE Leave (
 	final_approval_status varchar (50)
 
 	)
+
+GO
+
+CREATE VIEW allEmployeeProfiles AS
+
+SELECT employee_ID, first_name,last_name, gender, email, address, years_of_experience,
+official_day_off,type_of_contract,employment_status,annual_balance, accidental_balance 
+
+FROM Employee 
